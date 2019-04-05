00fe710f268ba7355a6cc29d8b5bf1be05fad690
William <williaamt0@gmail.com>
2019-04-05 12:38:31 -0400

    Update Evaluator.java

--- src/math/Evaluator.java
+++ src/math/Evaluator.java
@@ -1,9 +1,40 @@
 package math;
 
+import LogRegress;
+
 public class Evaluator
 {
-	public static double getAccuracy
+	public static double[] computePredictions(double[][]xArray)
 	{
+		LogRegress lr = new LogRegress;
+		
+		
+	}
+	
+	
+	public static double getAccuracy(double[]yLabels, double[]predictedYLabels);
+	{
+		double good = 0;
+		double bad = 0;
+		double accuracy = 0;
+		
+		for (int i = 0; i < yLabels.length; i++)
+		{
+			if (yLabels[i] == predictedYLabels[i])
+			{
+				good++;
+			}
+			else
+			{
+				bad++;
+			}
+		}
+		
+		
+		
+		
+		accuracy = (good/(good+bad));
+		return accuracy;
 	}
 	
 }
