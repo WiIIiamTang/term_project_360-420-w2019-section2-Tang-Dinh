2dccc6357f00acb3702d94921a69ea5bb9af58e2
William Tang <williaamt0@gmail.com>
2019-05-13 16:33:10 -0400

    average + standard deviation

--- src/MultipleRuns.java
+++ src/MultipleRuns.java
@@ -23,6 +23,7 @@ public class MultipleRuns
 
     ArrayList<Double> allTrueLabels = new ArrayList<Double>();
     ArrayList<Double> allPredictions = new ArrayList<Double>();
+    ArrayList<Double> allAccuracies = new ArrayList<Double>();
 
     for (int i = 0; i < Integer.parseInt(args[1]); i++)
     {
@@ -43,7 +44,7 @@ public class MultipleRuns
       LogisticRegression classifier = new LogisticRegression(x_train, x_test, y_train, y_test);
 
 
-      classifier.fit(0.01,10000,false); //alpha, maxiterations, randomize intial weights or not, check for tolerance level
+      classifier.fit(0.01,5000,false); //alpha, maxiterations, randomize intial weights or not, check for tolerance level
 
       double[] predictionsOnTrainSet = classifier.predictTrainSet(0.5);
       double[] predictionsOnTestSet = classifier.predictTestSet(0.5);
@@ -55,6 +56,8 @@ public class MultipleRuns
       }
 
       System.out.println("Running... " + i );
+
+      allAccuracies.add(ModelEvaluator.getAccuracy(y_test, predictionsOnTestSet));
     }
 
     double[] allLabels = new double[allTrueLabels.size()];
@@ -66,10 +69,41 @@ public class MultipleRuns
       allPredictedLabels[x] = allPredictions.get(x);
     }
 
+    System.out.println("The average accuracy is " + average(allAccuracies));
+    System.out.println("The standard deviation is " + sd(allAccuracies));
+
+    System.out.println("\nHere is the confusion matrix: ");
+
     ModelEvaluator.confusionMatrix(allLabels, allPredictedLabels);
 
 
 
 
+  }
+
+
+  public static double average(ArrayList<Double> x)
+  {
+    double sum = 0;
+
+    for (int i = 0; i < x.size(); i++)
+    {
+      sum = sum + x.get(i);
+    }
+
+    return sum/x.size();
+  }
+
+  public static double sd (ArrayList<Double> x)
+  {
+    double average = average(x);
+    double sum = 0;
+
+    for (int i = 0; i < x.size(); i++)
+    {
+      sum = sum + Math.pow(x.get(i) - average, 2);
+    }
+
+    return sum/x.size();
   }
 }
